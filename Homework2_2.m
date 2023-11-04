67c19d0e001dcfeb48cf345fb114c8c7f69893c2
Mohammad Mahdi Elyasi <lordmahdi79@gmail.com>
2023-11-04 16:08:48 +0330

    Comment details of Homework_1

--- Homework2_2.m
+++ Homework2_2.m
@@ -8,34 +8,73 @@ close all;
 clear ;
 clc;
 %% Homework_1
-
+% * Main purpose of this task is scrambling voice and reconstruct it
+%
+% * Now we import audio and plot it
 [x_t, fs] = audioread('Audio01.wav');
 x_t = x_t';
 t_axis = linspace(0, length(x_t) / fs, length(x_t));
 f0 = 10000;
 % plot
+figure('Name', 'Audio');
 plot(t_axis, x_t);
+xlabel('Samples');
+ylabel('Amplitude');
+title('time domain of Audio');
+grid on;
 
+figure('Name', 'Audio');
 plot(fftshift(abs(fft(x_t))));
-
-% sound(x_t, fs);
-% pause(length(x_t) / fs);
-
+xlabel('Samples');
+ylabel('Amplitude');
+title('fft of Audio');
+grid on;
+%%%
+% Here we hear the sound of audio
+sound(x_t, fs);
+pause(length(x_t) / fs);
+%%%
+% Now we declare needed variables and signals and import the
+% constructed filter by filterDesigner tool
 s = 2 * cos((2 * pi * f0) * t_axis);
 FD_FIR = filter_FIR;
 y = FD_FIR.filter(x_t);
+figure('Name', 'Signal');
 plot(t_axis, y);
+xlabel('Samples');
+ylabel('Amplitude');
+title('Filtered Signal');
+grid on;
+
 y1 = y .* s;
+
+figure('Name', 'Signal');
 plot(t_axis, y1);
+xlabel('Samples');
+ylabel('Amplitude');
+title('Shifted Signal');
+grid on;
+
 y2 = FD_FIR.filter(y1);
 imp_filter = load('filter.mat').Num;
 reso_freq = 10000;
 f_axis = linspace(-fs / 2, fs / 2, reso_freq);
-% f_axis1 = linspace(-fs / 2, fs / 2, size(imp_filter));
-
+%%%
+% y is filtered signal of input
+%
+% y1 is product of filtered signal and carrier
+%
+% y2 is filtered signal of y1
+figure('Name', 'Filter');
 plot(f_axis, fftshift(abs(fft(imp_filter, reso_freq))) / fs);
-% sound(y2, fs);
-% pause(length(y2) / fs);
+xlabel('Samples');
+ylabel('Amplitude');
+title('Absolute of Filter');
+grid on;
+%%%
+% Here we hear the sound of shifted signal
+sound(y2, fs);
+pause(length(y2) / fs);
 
 figure('Name', 'Signals');
 subplot(4, 2, 1)
@@ -106,14 +145,15 @@ subplot(2, 1, 2);
 plot(f_axis, unwrap(angle(fftshift(fft(imp_filter, reso_freq)))));
 xlabel('Samples');
 ylabel('Amplitude');
-% xlim([0 25000]);
 grid on;
 title('phase');
-% 2.4
+
 x2 = y2 .* s;
 x4 = filter(imp_filter, 1, x2);
-% sound(x4, fs);
-% pause(length(x4) / fs);
+%%%
+% Here We hear the sound of reconstructed signal
+sound(x4, fs);
+pause(length(x4) / fs);
 
 %% Homework_2
 
@@ -125,7 +165,7 @@ x_2 = chirp(t, 400, 1, 200);
 x_3 = 50 * [zeros(1, 249) 1 zeros(1, 751)];
 x3 = x_1 + x_2 + x_3;
 figure('Name', 'FFT');
-plot(abs(fft(x)));
+plot(abs(fftshift(fft(x3))));
 xlabel('freq(Hz)');
 ylabel('amp');
 title('fft');
